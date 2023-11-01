efc6d849dd4bca112051eb5e70b391653f93ce0a
Ruyangchen <3226717170@qq.com>
2023-11-01 09:57:46 +0800

    update

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/compactions/LocalOptimumCompactionPolicy.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/compactions/LocalOptimumCompactionPolicy.java
@@ -9,6 +9,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Class to pick which files if any to compact together. This class will do batch compaction
+ * based on local optimum and if it gets stuck it will choose the smallest set of files to compact.
+ */
 public class LocalOptimumCompactionPolicy extends RatioBasedCompactionPolicy {
   private static final Logger LOG = LoggerFactory.getLogger(LocalOptimumCompactionPolicy.class);
   public LocalOptimumCompactionPolicy(final Configuration conf,
@@ -16,18 +20,20 @@ public class LocalOptimumCompactionPolicy extends RatioBasedCompactionPolicy {
     super(conf, storeConfigInfo);
   }
 
-  @Override protected ArrayList<HStoreFile> applyCompactionPolicy(ArrayList<HStoreFile> candidates,
+  @Override
+  protected ArrayList<HStoreFile> applyCompactionPolicy(ArrayList<HStoreFile> candidates,
     boolean mayUseOffPeak, boolean mightBeStuck) {
     return new ArrayList<>(applyCompactionPolicy(candidates, mightBeStuck, mayUseOffPeak,
       comConf.getMinFilesToCompact(), comConf.getMaxFilesToCompact()));
   }
 
-  @Override public boolean needsCompaction(Collection<HStoreFile> storeFiles,
-    List<HStoreFile> filesCompacting) {
+  @Override
+  public boolean needsCompaction(Collection<HStoreFile> storeFiles, List<HStoreFile> filesCompacting) {
     ArrayList<Long> throttleValues = new ArrayList<>();
     long totalSize = getTotalStoreSize(storeFiles);
-    double meanSize = totalSize / (long) storeFiles.size();
-    int level = (int) (meanSize / storeConfigInfo.getMemStoreFlushSize());
+    long totalSize1 = getTotalStoreSize(filesCompacting);
+    double averageSize = (totalSize - totalSize1) / (long) (storeFiles.size() - filesCompacting.size());
+    int level = (int) (averageSize / storeConfigInfo.getMemStoreFlushSize());
     return totalSize > throttleValues.get(level);
   }
 
